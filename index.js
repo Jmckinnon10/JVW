c74b7f24c0f47c7cf6831a43531eae16e2e64a65
John McKinnon <johnmckinnon23@yahoo.com>
2023-03-04 13:04:19 -0700

    dakr mode and light mode working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let themeDots = document.getElementsByClassName('theme-dot')
 for(var i = 0; themeDots.length > i; i++){
   themeDots[i].addEventListener('click', function(){
     let mode = this.dataset.mode
-    console.log('clicked:', mode)
+    setTheme(mode)
   })
 }
 
